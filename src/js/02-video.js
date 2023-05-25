2f0b0c5a4223917103a7fd22bb5516d1bef032f3
Andriy2016 <122678717+Andriy2016@users.noreply.github.com>
2023-05-25 14:20:12 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,15 @@
 
-// import throttle from 'lodash.throttle';
-// import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-// player.on(
-//   'timeupdate',
-//   throttle(e => {
-//     localStorage.setItem('videoplayer-current-time', e.seconds);
-//   }, 1000)
-// );
+player.on(
+  'timeupdate',
+  throttle(e => {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+  }, 1000)
+);
 
-// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
